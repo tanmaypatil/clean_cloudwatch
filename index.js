330e2d6fe285cc97ac4e6ed5c3c12d13cac593a8
Tanmay Patil <tany.patil77@gmail.com>
2022-01-29 16:11:29 +0530

    deleting log stream

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 exports.handler = async (event) => {
-    const { CloudWatchLogsClient, DeleteLogStreamCommand,DescribeLogStreamsCommand } = require("@aws-sdk/client-cloudwatch-logs");
-    const client = new CloudWatchLogsClient({ region: "ap-south-1"  });
+    const { CloudWatchLogsClient, DeleteLogStreamCommand, DescribeLogStreamsCommand } = require("@aws-sdk/client-cloudwatch-logs");
+    const client = new CloudWatchLogsClient({ region: "ap-south-1" });
     console.log('getting list of log streams');
     let nextToken = null;
-    do  {
-    let  input_command = {
-         logGroupName : '/aws/lambda/send-alarms' ,
-         nextToken : nextToken,
-         limit : 10
-    };
-    const command = new DescribeLogStreamsCommand(input_command);
-    const resp_cmd = await client.send(command);
-   // console.log('nextToken '+resp_cmd.nextToken);
-    console.log('log streams '+JSON.stringify(resp_cmd.logStreams));
-    nextToken = resp_cmd.nextToken;
-    } while ( nextToken != null);
+    let logStreams = []
+    do {
+        let input_command = {
+            logGroupName: '/aws/lambda/send-alarms',
+            nextToken: nextToken,
+            limit: 10
+        };
+        let command = new DescribeLogStreamsCommand(input_command);
+        let resp_cmd = await client.send(command);
+        console.log('log streams ' + JSON.stringify(resp_cmd.logStreams));
+        nextToken = resp_cmd.nextToken;
+        logStreams.concat(resp_cmd.logStreams);
+    } while (nextToken != null);
 
-    
+    if (logStreams.length > 0) {
+        for (let logStream of logStreams) {
+            let input = {
+                logGroupName: '/aws/lambda/send-alarms',
+                logStreamName: logStream.logStreamName
+            }
+
+            let command = new DeleteLogStreamCommand(input);
+            let resp_cmd = await client.send(command);
+        }
+    }
+
     // TODO implement
     const response = {
         statusCode: 200,
